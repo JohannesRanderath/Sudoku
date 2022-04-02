5b710796dae87341a5662c6e904bc6ed2561c471
Johannes Randerath <joranderathsg@gmail.com>
2022-04-02 19:06:23 +0200

    Added readme and more docstrings

---
+++ README.rst
@@ -0,0 +1,14 @@
+===================
+Sudoku
+===================
+A simple Sudoku game.
+
+The game is in german, feel free to add localization.
+
+.. image:: game.png
+
+Starting the game
+_____________________
+The game is built in Python with Tkinter, meaning it is cross-platform. To open it on your platform, make
+*play_sudoku.command* an executable python file for your system (or just execute `python main.py` on the command line).
+On MacOS it is executable, so just double click the file.
--- main.py
+++ main.py
@@ -9,26 +9,45 @@ def routine():
     """
     Draws GUI application with Tkinter, starts new game
     """
-    my_sudoku = []
+    my_sudoku = []  # sudoku grid used for current game
+    solution = []
+
+    # default constraints
+    field_width = 5
+    field_height = 2
+    field_font = ("default", 30)
+    bg_color = "white"
 
     def game():
         """
-        Draws grid for own specific game.
+        Draws grid for one specific game.
         :return: None
         """
         nonlocal my_sudoku
-        active_field = None
+        active_field = None  # cell selected by click
 
-        elements = [[] for i in range(9)]
+        elements = [[] for i in range(9)]  # Cells of the grid
 
-        def change_active_field(field, r, c):
+        def change_active_field(r, c):
+            """
+            Emulated Click handler. Selects cell the user clicks on by changing its color and making it the active field
+            :param r: row of field clicked on
+            :param c: column of the field
+            :return: None
+            """
             nonlocal active_field
-            if active_field:
-                elements[active_field["row"]][active_field["column"]].config(bg="white")
-            active_field = {"field": field, "row": r, "column": c}
+            if active_field:  # reset color of last active field if one exists
+                elements[active_field["row"]][active_field["column"]].config(bg=bg_color)
+            active_field = {"row": r, "column": c}
             elements[r][c].config(bg="#E5E7E9")
 
         def change_number(event):
+            """
+            Key press handler. If a number is pressed and a cell is selected, the number is filled in to the cell.
+            If backspace is pressed and a cell is selected, the cell's value is deleted.
+            :param event: key press event, that occured.
+            :return: None
+            """
             if active_field:
                 if event.keysym == "BackSpace":
                     elements[active_field["row"]][active_field["column"]].config(text="")
@@ -39,29 +58,35 @@ def routine():
 
         window.bind("<Key>", change_number)
 
+        args = {"width": field_width, "bg": bg_color, "font": field_font,
+                "height": field_height}  # basic arguments for cell's Label
+        # Generate grid GUI with sudoku
         for i, row in enumerate(my_sudoku):
             for j, column in enumerate(row):
-                frm = tk.Frame(
+                frm = tk.Frame(  # Every cell should have a border
                     highlightbackground="black",
                     highlightthickness=1,
                     master=grid_frm,
                     height=field_height,
                     width=field_width
                 )
-                args = {"master": frm, "width": field_width, "background": "white", "font": field_font, "height": field_height}
-                if column == 0:
+
+                args["master"] = frm
+                if column == 0:  # Labels of empty cells should not have any text and should be selected on click.
                     el = tk.Label(
                         **args
                     )
-                    el.bind("<Button-1>", lambda event, field=el, r=i, c=j: change_active_field(el, r, c))
-                else:
+                    el.bind("<Button-1>", lambda event, r=i, c=j: change_active_field(r, c))
+                else:  # Labels of cells filled by default should display the number
                     el = tk.Label(
                         **args,
                         text=column
                     )
                 elements[i].append(el)
+                # Make sudoku 3x3 squares visible by adding some space between them
                 padding_top = 5 if i % 3 == 0 else 0
                 padding_left = 5 if j % 3 == 0 else 0
+                # Display cells in strict grid.
                 frm.grid(row=i, column=j, pady=(padding_top, 0), padx=(padding_left, 0))
                 el.pack()
 
@@ -69,13 +94,20 @@ def routine():
         tk.mainloop()
 
     window = tk.Tk()
-    frm_control_btns = tk.Frame(
-        bg="white"
+    window.title("Sudoku")
+    frm_control_btns = tk.Frame(  # Upper row of buttons to control the game
+        bg=bg_color
     )
+    # Slider to set difficulty in percentage of masked cells. Default is 50, maximum is 90.
     scale_dif = tk.Scale(master=frm_control_btns, from_=1, to=9, orient=tk.HORIZONTAL)
     scale_dif.set(5)
 
+    # Click listener and widget for New game button
     def new_game():
+        """
+        Generates a new sudoku and starts the game.
+        :return: None
+        """
         nonlocal my_sudoku
         nonlocal solution
         my_sudoku = sudoku.generate_sudoku(scale_dif.get()*10)
@@ -84,25 +116,36 @@ def routine():
 
     btn_new_game = tk.Button(
         text="Neues Sudoku",
-        background="white",
+        bg=bg_color,
         master=frm_control_btns,
         command=new_game
     )
 
+    # Click listener and widget for button to check if the solution is valid
     def check():
+        """
+        Checks solution from GUI and displays a success or error message.
+        :return: None
+        """
         if sudoku.check(solution):
-            messagebox.showinfo(title="Richtig!", message="Herzlichen Glückwunsch! \nDu hast das Sudoku richtig gelöst!")
+            messagebox.showinfo(title="Richtig!", message="Herzlichen Glückwunsch! "
+                                                          "\nDu hast das Sudoku richtig gelöst!")
         else:
             messagebox.showerror(title="Leider falsch", message="Leider noch nicht ganz richtig :(")
 
     btn_check = tk.Button(
         text="Überprüfen",
-        background="white",
+        bg=bg_color,
         master=frm_control_btns,
         command=check
     )
 
+    # Click listener and widget for reset button
     def reset():
+        """
+        Resets the grid and the solution array to the originally created sudoku
+        :return: None
+        """
         nonlocal solution
         nonlocal my_sudoku
         solution = [row[:] for row in my_sudoku]
@@ -110,17 +153,20 @@ def routine():
 
     btn_reset = tk.Button(
         text="Zurücksetzen",
-        background="white",
+        bg=bg_color,
         master=frm_control_btns,
         command=reset
 
     )
 
+    # Label to indicate purpose of difficulty slider.
     scale_lbl = tk.Label(
         text="Schwierigkeit: ",
-        background="white",
+        bg=bg_color,
         master=frm_control_btns
     )
+
+    # pack control elements
     scale_lbl.pack(side=tk.LEFT)
     scale_dif.pack(side=tk.LEFT)
     btn_new_game.pack(side=tk.LEFT)
@@ -129,13 +175,10 @@ def routine():
 
     frm_control_btns.pack(fill=tk.X)
 
-    grid_frm = tk.Frame(bg="white")
+    # frame containing the game grid
+    grid_frm = tk.Frame(bg=bg_color)
 
-    field_width = 5
-    field_height = 2
-    field_font = ("default", 30)
-
-    solution = []
+    # start a new game and initialize grid on startup
     new_game()
 
 
